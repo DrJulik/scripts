3ba917691898d764ce88691db916aac75b892cdc
Yuliy Sozin <j.batkovich@gmail.com>
2020-10-06 14:25:54 -0400

    1

--- modal.js
+++ modal.js
@@ -2,10 +2,13 @@ if (
 	window.location.href ===
 	"https://easypop-development.myshopify.com/collections/all"
 ) {
+	const body = document.querySelector("body");
 	const modal = document.createElement("div").classList.add("fullscreen_popup");
 	const popup_content = document
 		.createElement("div")
 		.classList.add("popup_content");
 
 	modal.appendChild(popup_content);
+
+	body.appendChild(modal);
 }
