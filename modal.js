eaadf19b9404a489d61b4f46c0f662fe40b0e076
Yuliy Sozin <j.batkovich@gmail.com>
2020-10-06 14:18:10 -0400

    1

--- modal.js
+++ modal.js
@@ -1,3 +1,11 @@
-setTimeout(() => {
-	alert("You are the best!");
-}, 3000);
+if (
+	window.location.href ===
+	"https://easypop-development.myshopify.com/collections/all"
+) {
+	const modal = document.createElement("div").classList.add("fullscreen_popup");
+	const popup_content = document
+		.createElement("div")
+		.classList.add("popup_content");
+
+	modal.appendChild(popup_content);
+}
